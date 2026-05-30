735aba2e073763bd5bbc37cb91ff216d63aea54f
Dawood Nadeem <Captaindawood12@gmail.com>
2026-05-30 14:46:42 +0800

    Fix Supabase URL typo

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL  = "https://dlcgszsinmznnljwijq.supabase.co";
+const SUPABASE_URL  = "https://dlcgszsinmznnjljwijq.supabase.co";
 const SUPABASE_KEY  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsY2dzenNpbm16bm5qbGp3aWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2ODkxMTYsImV4cCI6MjA5NTI2NTExNn0.iy_MNAKNWNge0gUCot-PtVkZdecfEKv3EeMdf86XheA";
 const OPENAI_KEY    = "not-needed";
 const OPENAI_MODEL  = "gpt-4o-mini";
